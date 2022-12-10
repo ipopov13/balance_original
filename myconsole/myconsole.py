1426c729b6c9cd81094e3374c4d9b7c26241f917
Ivan Popov <thema3xman@gmail.com>
2022-12-10 20:57:19 +0200

    Added color codes

--- myconsole/myconsole.py
+++ myconsole/myconsole.py
@@ -4,7 +4,22 @@ from console.utils import cls, set_title
 
 class Console:
     """Adapter class for Console"""
-
+    colors = {0: console.fg.black,
+        1: console.fg.blue,
+        2: console.fg.green,
+        3: console.fg.lightblue,
+        4: console.fg.red,
+        5: console.fg.purple,
+        6: console.fg.t_a8a800,
+        7: console.fg.lightblack,
+        8: console.fg.t_545454,
+        9: console.fg.t_5454fc,
+        10: console.fg.t_54fc54,
+        11: console.fg.t_54fcfc,
+        12: console.fg.t_fc5454,
+        13: console.fg.t_fc54fc,
+        14: console.fg.yellow,
+        15: console.fg.white}
     location = console.screen.sc.location
 
     @staticmethod
@@ -38,12 +53,12 @@ class Console:
     @staticmethod
     def scroll(coords, i0, i1, color, char):
         with Console.location(coords[1], coords[0]):
-            print(char)
+            print(f'{Console.colors[color]}{char}{console.fx.end}')
 
     @staticmethod
     def text(x, y, text, color):
         with Console.location(y, x):
-            print(text)
+            print(f'{Console.colors[color]}{text}{console.fx.end}')
 
 # cls()
 # with console.screen.sc.location(0, 0):
