daa8dcf4287d56e98341364a265f83f290d0e76b
Ivan Popov <thema3xman@gmail.com>
2022-12-14 21:24:31 +0200

    Added TODOs

--- restructured/windows.py
+++ restructured/windows.py
@@ -2,18 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class Window(ABC):
+    _size = (25, 80)
+    _top_left = (0, 0)
 
     @abstractmethod
     def _commands(self) -> dict:
         """The mapping of commands&methods specific for the window"""
-        return {}
-
-    def get_display_data(self):
-        return self._format_display_data()
+        return {'?': self._help_command}
 
     @abstractmethod
-    def _format_display_data(self):
-        pass
+    def _collect_content_data(self):
+        raise NotImplementedError
 
     @abstractmethod
     def _content_commands(self) -> dict:
@@ -21,13 +20,24 @@ class Window(ABC):
         #TODO: Add automatic command collection here, drop method from children
         pass
 
+    def get_display_data(self):
+        content_data = self._collect_content_data()
+        #TODO: Pad the content data to the screen size
+        #TODO: Adjust the coordinates of the display data to the UI coordinate
+        # system using the _top_left coordinates
+
+    def _available_commands(self) -> dict:
+        return {**self._commands(), **self._content_commands()}
+
     @staticmethod
     def _empty_command(_):
         return {(0, 0): ''}
 
+    def _help_command(self):
+        return {(0, 0): 'help command'}
+
     def handle_input(self, player_input) -> dict:
-        available_commands = {**self._commands(), **self._content_commands()}
-        chosen_command = available_commands.get(player_input, self._empty_command)
+        chosen_command = self._available_commands().get(player_input, self._empty_command)
         updates = chosen_command(player_input)
         return updates
 
