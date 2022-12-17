f956cdd30382b2cb4dece2720aef92b37bd0bcf8
Ivan Popov <thema3xman@gmail.com>
2022-12-17 09:35:54 +0200

    Implemented window titles and border

--- restructured/windows.py
+++ restructured/windows.py
@@ -2,26 +2,25 @@ from abc import ABC, abstractmethod
 from content_types import CommandsList
 import commands
 
-# TODO: A window should check for overlapping commands and raise an error!
-# TODO: A window can have a border and have the command hints and title integrated in it
-
 
 class Window(ABC):
     _default_size = (25, 80)
     _default_top_left = (0, 0)
 
-    def __init__(self, size=_default_size, top_left=_default_top_left, ui=None, contents=()):
+    def __init__(self, size=_default_size, top_left=_default_top_left, ui=None,
+                 contents=(), border=False, title='Balance'):
         if ui is None:
             raise ValueError(f"Window {self.__class__} must be initialized with a UI!")
         self.ui = ui
         self.size = size
         self.top_left = top_left
         self._contents = contents
+        self._border = border
+        self._title = title
 
-    @abstractmethod
     def _commands(self) -> dict:
         """The mapping of commands&methods specific for the window"""
-        raise NotImplementedError(f'Window {self.__class__} must define a _commands() method!')
+        return {commands.GetHelp(): self._help_command}
 
     @abstractmethod
     def _organize_content_data(self) -> str:
@@ -31,34 +30,64 @@ class Window(ABC):
     def _content_commands(self) -> dict:
         """The mapping of commands&methods specific for the window content"""
         content_commands = {}
+        # TODO: A window should check for overlapping commands and raise an error!
         for content in self._contents:
             content_commands.update(content.commands())
         return content_commands
 
     def get_display_data(self) -> dict:
-        """Pad the content to size and position and return it to the UI"""
+        """Pad the content to size and position, apply borders and hints"""
+        # TODO: For composite windows (i.e. game scene) this can be called by a provide_data method that sends
+        #  the contents of the separate sub-windows one by one (so this method would be private). Border
+        #  should be only one though, if any
         content_data = self._organize_content_data()
         content_data = content_data.split('\n')
+        # Check if all content fits the line length, add ellipsis
         for row_index in range(len(content_data)):
-            content_data[row_index] += ' ' * (self.size[1] - len(content_data[row_index]))
-        content_data += [' ' * self.size[1]] * (self.size[0] - len(content_data))
-        content_data[-1] = '(?)' + content_data[-1][3:]
+            if len(content_data[row_index]) > self.size[-1]:
+                content_data[row_index] = content_data[row_index][:self.size[-1] - 3] + '...'
+        # Horizontal pad: center the longest content line
+        longest_line_len = max([len(line) for line in content_data])
+        left_pad = (self.size[-1] - longest_line_len) // 2
+        min_right_pad = self.size[-1]
+        for row_index in range(len(content_data)):
+            content_data[row_index] = ' ' * left_pad + content_data[row_index]
+        for row_index in range(len(content_data)):
+            right_pad = (self.size[1] - len(content_data[row_index]))
+            content_data[row_index] += ' ' * right_pad
+            min_right_pad = min(right_pad, min_right_pad)
+        # Vertical pad: center within window size
+        top_pad = (self.size[0] - len(content_data)) // 2
+        bottom_pad = self.size[0] - len(content_data) - top_pad
+        content_data = [' ' * self.size[1]] * top_pad + content_data
+        content_data += [' ' * self.size[1]] * bottom_pad
+        if self._border:
+            content_data = self._apply_border(content_data,
+                                              pads=[left_pad, min_right_pad, top_pad, bottom_pad])
         content_dict = {(row_index + self.top_left[0], 0 + self.top_left[1]): row
                         for row_index, row in enumerate(content_data)}
         return content_dict
 
+    def _apply_border(self, content_data, pads):
+        # TODO: A window can have a border and have the command hints and title integrated in it
+        if not all([p > 0 for p in pads]):
+            raise ValueError(f'Content is too big to apply border in {self.__class__}')
+        content_data[0] = self._title.center(self.size[-1], '-')
+        content_data[-1] = '-' * self.size[-1]
+        # content_data = self._apply_hints(content_data)
+        return content_data
+
     def _available_commands(self) -> dict:
-        return {**self._commands(),
-                **self._content_commands(),
-                **{commands.GetHelp(): self._help_command}}
+        # TODO: A window should check for overlapping commands and raise an error!
+        return {**self._commands(), **self._content_commands()}
 
     def _empty_command(self, _) -> bool:
-        # TODO Make this an actual DoNothing
         return self.ui.display({(0, 0): ''})
 
     def _help_command(self, _) -> bool:
         help_window = OverlayWindow(size=(15, 50), top_left=(5, 20), ui=self.ui,
-                                    contents=[CommandsList(self._available_commands())])
+                                    contents=[CommandsList(self._available_commands())],
+                                    border=True, title='Available commands')
         return self.ui.add_window(help_window)
 
     def handle_input(self, player_input) -> bool:
@@ -76,7 +105,9 @@ class WelcomeWindow(Window):
         pass
 
     def _commands(self):
-        return {commands.NewGame(): self._new_game, commands.LoadGame(): self._load_game}
+        return {commands.NewGame(): self._new_game,
+                commands.LoadGame(): self._load_game,
+                commands.GetHelp(): self._help_command}
 
     def _new_game(self, _):
         return self.ui.display({(0, 0): 'new game '})
